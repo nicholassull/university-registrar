2bdb28e01797a5ac4af71733559d337a7c6d3914
Nick Sullivan <nicholassull@gmail.com>
2022-03-16 19:21:50 -0700

    add delete functionality for courses
    Co-authored-by: Anastasia Han <anastasia.han@yahoo.com>
    Co-authored-by: ChrisNakayama <cnakayam@gmail.com>

--- UniversityRegistrar/Controllers/CoursesController.cs
+++ UniversityRegistrar/Controllers/CoursesController.cs
@@ -49,5 +49,18 @@ namespace UniversityRegistrar.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult Delete(int id)
+    {
+      var thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      _db.Courses.Remove(thisCourse);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
